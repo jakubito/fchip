0b7c9e12d8e05089b8272294f942a428e6516b8b
Jakub Dobes <dobes.jakub@gmail.com>
2022-05-24 18:25:17 -0700

    Add fullscreen support

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,15 +1,35 @@
-import { useEffect, useRef } from 'preact/hooks'
+import { useEffect, useRef, useState } from 'preact/hooks'
 import { useClient } from '../clientContext'
 
 function Display() {
   const client = useClient()
   const display = useRef<HTMLDivElement>(null)
+  const [fullscreen, setFullscreen] = useState(false)
 
   useEffect(() => {
     client.appendCanvasTo(display.current!)
+
+    document.addEventListener('fullscreenchange', () => {
+      setFullscreen(Boolean(document.fullscreenElement))
+    })
   }, [])
 
-  return <div id="display" class="d-flex justify-content-center align-items-center" ref={display} />
+  return (
+    <div class="d-flex flex-column align-items-center mb-4">
+      <div
+        id="display"
+        class={`d-flex justify-content-center align-items-center ${fullscreen ? 'fullscreen' : ''}`}
+        ref={display}
+      />
+      <button
+        type="button"
+        class="btn btn-light"
+        onClick={() => display.current?.requestFullscreen({ navigationUI: 'hide' })}
+      >
+        <i class="bi bi-fullscreen" /> Full screen
+      </button>
+    </div>
+  )
 }
 
 export default Display
